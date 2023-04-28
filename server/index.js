3fadfacd04cd3a1c6e23984fa4adb4b859893391
Joe Kim <joesm.genie@gmail.com>
2023-04-28 12:05:34 +0700

    Project & Client queries

---
+++ server/index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const colors = require('colors');
+const expresss = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+const connectDB = require('./config/db');
+
+const PORT = process.env.PORT || 5000;
+
+const app = expresss();
+
+// Connect to database
+connectDB();
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: process.env.NODE_ENV === 'development',
+}))
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
